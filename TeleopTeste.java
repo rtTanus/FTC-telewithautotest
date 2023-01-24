3748c1a866d57a667b7b9a0e02217300b36c092b
Tanus <120686364+rtTanus@users.noreply.github.com>
2023-01-23 22:23:52 -0300

    Update TeleopTeste.java

--- TeleopTeste.java
+++ TeleopTeste.java
@@ -13,7 +13,7 @@ public class TeleopTeste extends OpMode{
 
     @Override
     public void init(){
-        motorEsquerdoF  = hardwareMap.get(DcMotor.class, "LeftDriveUp");
+        motorEsquerdoF  = hardwareMap.get(DcMotor.class, "leftDriveUp");
         motorDireitoF  = hardwareMap.get(DcMotor.class, "RightDriveUp");
         motorEsquerdoT = hardwareMap.get(DcMotor.class, "LeftDriveDown");
         motorDireitoT = hardwareMap.get(DcMotor.class, "RightDriveDown");
@@ -31,7 +31,7 @@ public class TeleopTeste extends OpMode{
         Arm = hardwareMap.get(DcMotor.class, "Arm");
         Arm.setDirection(DcMotor.Direction.FORWARD);
 
-        servoMotor = hardwareMap.get(Servo.class, "Serv");
+        servoMotor = hardwareMap.get(Servo.class, "Arm");
 
         resetRuntime();
     }
@@ -39,39 +39,84 @@ public class TeleopTeste extends OpMode{
         linear();
         servo();
         mover();
+        telemetry.update();
     }
     public void mover(){
         double axial   = -gamepad1.left_stick_y;
         double lateral =  gamepad1.left_stick_x;
         double yaw     =  gamepad1.right_stick_x;
+        double backDirection = gamepad1.right_trigger;
+        double veltonotturb = 0.3;
         double absaxial = Math.abs(axial);
         double abslateral = Math.abs(lateral);
         double absyaw= Math.abs(yaw);
         double denominador = Math.max(absaxial + abslateral + absyaw, 1);
+        boolean boostpower = gamepad1.right_bumper;
 
 
-        double motorEsquerdoFf  = axial + lateral + yaw / denominador;
-        double motorDiretoFf = axial - lateral - yaw / denominador;
-        double motorEsquerdoTf   = axial - lateral + yaw / denominador;
-        double motorDireitoTf  = axial + lateral - yaw / denominador;
+        double motorEsquerdoFf  = (axial + lateral + yaw / denominador) - veltonotturb;
+        double motorDiretoFf = (axial - lateral - yaw / denominador) - veltonotturb;
+        double motorEsquerdoTf   = (axial - lateral + yaw / denominador) - veltonotturb;
+        double motorDireitoTf  = (axial + lateral - yaw / denominador) - veltonotturb;
 
-        double motorEsquerdoFt  = axial - lateral - yaw / denominador;
-        double motorDiretoFt = axial + lateral + yaw / denominador;
-        double motorEsquerdoTt   = axial + lateral - yaw / denominador;
-        double motorDireitoTt  = axial - lateral + yaw / denominador;
+        double motorEsquerdoFt  = (axial - lateral - yaw / denominador) - veltonotturb;
+        double motorDiretoFt = (axial + lateral + yaw / denominador) - veltonotturb;
+        double motorEsquerdoTt   = (axial + lateral - yaw / denominador) - veltonotturb;
+        double motorDireitoTt  = (axial - lateral + yaw / denominador) - veltonotturb;
 
-        if (!gamepad1.a){
-            allMotorsPower(motorEsquerdoFf,motorDiretoFf,motorEsquerdoTf,motorDireitoTf);
+        if (backDirection <= 0.1){
+            if (!boostpower)
+
+                allMotorsPower(motorEsquerdoFf,motorDiretoFf,motorEsquerdoTf,motorDireitoTf);
+
+            telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoFf);
+            telemetry.addData("A potencia do motorDireitoF é de:", motorDiretoFf);
+            telemetry.addData("A potencia do motorEsquerdoT é de:", motorEsquerdoTf);
+            telemetry.addData("A potencia do motorDireitoT é de:", motorDireitoTf);
+
+            if (boostpower){
+
+                double motorEsquerdoFfr  = axial + lateral + yaw / denominador;
+                double motorDiretoFfr = axial - lateral - yaw / denominador;
+                double motorEsquerdoTfr   = axial - lateral + yaw / denominador;
+                double motorDireitoTfr  = axial + lateral - yaw / denominador;
+
+                allMotorsPower(motorEsquerdoFfr,motorDiretoFfr,motorEsquerdoTfr,motorDireitoTfr);
+
+                telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoFfr);
+                telemetry.addData("A potencia do motorDireitoF é de:", motorDiretoFfr);
+                telemetry.addData("A potencia do motorEsquerdoT é de:", motorEsquerdoTfr);
+                telemetry.addData("A potencia do motorDireitoT é de:", motorDireitoTfr);
+            }
         }
-        else if(gamepad1.a){
-            allMotorsPower(motorEsquerdoFt,motorDiretoFt,motorEsquerdoTt,motorDireitoTt);
+        else if(backDirection > 0.1){
+
+            if (!boostpower)
+
+                allMotorsPower(motorEsquerdoFt,motorDiretoFt,motorEsquerdoTt,motorDireitoTt);
+
+                telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoFt);
+                telemetry.addData("A potencia do motorDireitoF é de:", motorDiretoFt);
+                telemetry.addData("A potencia do motorEsquerdoT é de:", motorEsquerdoTt);
+                telemetry.addData("A potencia do motorDireitoT é de:", motorDireitoTt);
+                
+            if (boostpower){
+
+                double motorEsquerdoFtr  = axial + lateral + yaw / denominador;
+                double motorDiretoFtr = axial - lateral - yaw / denominador;
+                double motorEsquerdoTtr   = axial - lateral + yaw / denominador;
+                double motorDireitoTtr  = axial + lateral - yaw / denominador;
+
+                allMotorsPower(motorEsquerdoFtr,motorDiretoFtr,motorEsquerdoTtr,motorDireitoTtr);
+
+                telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoFtr);
+                telemetry.addData("A potencia do motorDireitoF é de:", motorDiretoFtr);
+                telemetry.addData("A potencia do motorEsquerdoT é de:", motorEsquerdoTtr);
+                telemetry.addData("A potencia do motorDireitoT é de:", motorDireitoTtr);
+                
+            }
         }
 
-
-        telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoFf);
-        telemetry.addData("A potencia do motorDireitoF é de:", motorDiretoFf);
-        telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoTf);
-        telemetry.addData("A potencia do motorEsquerdoF é de:", motorDireitoTf);
         telemetry.addData("O tempo em que o robô opera é de :", getRuntime());
         telemetry.update();
     }
@@ -98,24 +143,24 @@ public class TeleopTeste extends OpMode{
                 Arm.setPower(pow);
             }
         }
-        if (poderCima && powMax) {
+        if (poderCima && powMax == true) {
             pow = 1;
             Arm.setPower(pow);
         }
-        else if(poderBaixo && powMax) {
+        else if(poderBaixo && powMax == true) {
             pow = -1;
             Arm.setPower(pow);
         }
-        else if (poderCima && powMin){
+        else if (poderCima && powMin == true){
             pow = 0.1;
             Arm.setPower(pow);
         }
-        else if (poderBaixo && powMin){
+        else if (poderBaixo && powMin == true){
             pow = -0.1;
             Arm.setPower(pow);
         }
         telemetry.addData("A potencia do motor do sistema linear é de", pow);
-        telemetry.update();
+        
     }
     public void servo() {
         boolean poderAberto = gamepad2.a;
@@ -128,13 +173,11 @@ public class TeleopTeste extends OpMode{
             powServo = aberto;
             servoMotor.setPosition(powServo);
             telemetry.addData("A potencia do motor do servo é de:", powServo);
-            telemetry.update();
         }
         else if (poderFechado) {
             powServo = fechado;
             servoMotor.setPosition(powServo);
             telemetry.addData("A potencia do motor do servo é de:", powServo);
-            telemetry.update();
         }
     }
     public void allMotorsPower(double paMEF, double paMDF, double paMET, double paMDT){
