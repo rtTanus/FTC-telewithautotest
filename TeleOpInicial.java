6e3ab4cef83cf39554b6ce9e0b00683afca30c61
Tanus <120686364+rtTanus@users.noreply.github.com>
2023-01-31 15:54:38 -0300

    Update TeleOpInicial.java

--- TeleOpInicial.java
+++ TeleOpInicial.java
@@ -1,17 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name="TeleOpInicial", group="OpMode")
-public class TeleOpInicial extends OpMode{
-    public Servo servoMotor;
+@TeleOp(name="TesteTO", group="OpMode")
+public class TesteTO extends OpMode{
+    public Servo servoMotor = null;
     public DcMotor motorEsquerdoF, motorEsquerdoT, motorDireitoF, motorDireitoT = null;
-    public DcMotor Arm;
+    public DcMotor Arm = null;
 
     @Override
     public void init(){
@@ -20,90 +19,63 @@ public class TeleOpInicial extends OpMode{
         motorEsquerdoT = hardwareMap.get(DcMotor.class, "LeftDriveDown");
         motorDireitoT = hardwareMap.get(DcMotor.class, "RightDriveDown");
 
-        motorEsquerdoF.setDirection(DcMotor.Direction.FORWARD);
+        motorEsquerdoF.setDirection(DcMotor.Direction.REVERSE);
         motorDireitoF.setDirection(DcMotor.Direction.FORWARD);
         motorEsquerdoT.setDirection(DcMotor.Direction.REVERSE);
-        motorDireitoT.setDirection(DcMotor.Direction.REVERSE);
+        motorDireitoT.setDirection(DcMotor.Direction.FORWARD);
 
         Arm = hardwareMap.get(DcMotor.class, "Arm");
-
         Arm.setDirection(DcMotor.Direction.FORWARD);
 
-        servoMotor = hardwareMap.get(Servo.class, "Serv");
-
-        resetRuntime();
+        servoMotor = hardwareMap.get(Servo.class, "Servo");
 
 
     }
 
     public void loop(){
-        linear();
         servo();
         mover();
+        linear();
     }
 
     public void mover(){
-        double axial   = -gamepad1.left_stick_y;
-        double lateral =  gamepad1.left_stick_x;
+
+        double axial   = gamepad1.right_trigger - gamepad1.left_trigger;
+        double lateral = gamepad1.left_stick_x;
         double yaw     =  gamepad1.right_stick_x;
-
-        double denominador = Math.max(Math.abs(axial) + Math.abs(lateral) + Math.abs(yaw), 1);
-        double motorEsquerdoFt  = axial + lateral + yaw / denominador;
-        double motorDiretoFt = axial - lateral - yaw / denominador;
-        double motorEsquerdoTt   = axial - lateral + yaw / denominador;
-        double motorDireitoTt  = axial + lateral - yaw / denominador;
-
-        allMotorsPower(motorEsquerdoFt,motorDiretoFt,motorEsquerdoTt,motorDireitoTt);
-
-        telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoFt);
-        telemetry.addData("A potencia do motorDireitoF é de:", motorDiretoFt);
-        telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoTt);
-        telemetry.addData("A potencia do motorEsquerdoF é de:", motorDireitoTt);
-        telemetry.addData("O tempo em que o robô opera é de :", getRuntime());
-    }
-    public void linear() {
-        boolean poderCima = gamepad2.right_bumper;
-        boolean poderBaixo = gamepad2.left_bumper;
-        double pow = 0;
+        double absaxial = Math.abs(axial);
+        double abslateral = Math.abs(lateral);
+        double absyaw= Math.abs(yaw);
+        double denominador = Math.max(absaxial + abslateral + absyaw, 1);
 
 
-        if (poderCima) {
-            pow = pow + 0.1;
-            Arm.setPower(pow);
-            if (pow >= 1) {
-                Arm.setPower(1);
-            }
+        double motorEsquerdoFf = (axial + lateral + yaw / denominador);
+        double motorDireitoFf = (axial - lateral - yaw / denominador);
+        double motorEsquerdoTf = (axial - lateral + yaw / denominador);
+        double motorDireitoTf = (axial + lateral - yaw / denominador);
+
+        allMotorsPower(motorEsquerdoFf, motorDireitoFf, motorEsquerdoTf, motorDireitoTf);
+
+        if(gamepad1.dpad_down){
+            allMotorsPower(-0.35,-0.35,-0.35,-35);
         }
-        if (poderBaixo) {
-            pow = pow - 0.1;
-            Arm.setPower(pow);
-            if(pow <= -1);
-            Arm.setPower(-1);
-
-            telemetry.addData("A potencia do motor do sistema linear é de", pow);
-
-
+        if(gamepad1.dpad_up){
+            allMotorsPower(0.35,0.35,0.35,0.35);
         }
-    }
-    public void servo() {
-        boolean poderAberto = gamepad2.a;
-        boolean poderFechado = gamepad2.b;
-        double powServo;
-        double aberto = 0;
-        double fechado = 1;
-
-        if (poderAberto) {
-            powServo = aberto;
-            servoMotor.setPosition(powServo);
-
-        } else if (poderFechado) {
-            powServo = fechado;
-            servoMotor.setPosition(powServo);
-
-            telemetry.addData("A potencia do motor do servo é de:", powServo);
+        if(gamepad1.dpad_right){
+            allMotorsPower(0.35,-0.35,-0.35,0.35);
+        }
+        if(gamepad1.dpad_left){
+            allMotorsPower(-0.35,0.35,0.35,-0.35);
         }
 
-    }
+        telemetry.addData("A potencia do motorEsquerdoF é de:", motorEsquerdoFf);
+        telemetry.addData("A potencia do motorDireitoF é de:", motorDireitoFf);
+        telemetry.addData("A potencia do motorEsquerdoT é de:", motorEsquerdoTf);
+        telemetry.addData("A potencia do motorDireitoT é de:", motorDireitoTf);
+
+        }
+
 
     public void allMotorsPower(double paMEF, double paMDF, double paMET, double paMDT){
         motorEsquerdoF.setPower(paMEF);
@@ -111,6 +83,40 @@ public class TeleOpInicial extends OpMode{
         motorEsquerdoT.setPower(paMET);
         motorDireitoT.setPower(paMDT);
     }
+    public void linear() {
+        double poderCima = gamepad2.right_trigger;
+        double poderBaixo = gamepad2.left_trigger;
+        double pow = poderCima - poderBaixo;
 
+        Arm.setPower(pow);
 
+        if(gamepad1.right_bumper){
+            Arm.setPower(0.3);
+        }
+        if(gamepad1.left_bumper){
+            Arm.setPower(0.3);
+        }
+
+        telemetry.addData("A potencia do motor do sistema linear é de", pow);
+
+    }
+    double powServo = 0;
+
+    public void servo() {
+        boolean poderAberto = gamepad2.a;
+        boolean poderFechado = gamepad2.b;
+        double aberto = 0;
+        double fechado = 1;
+
+        if (poderAberto) {
+            powServo = aberto;
+            servoMotor.setPosition(powServo);
+        }
+        else if (poderFechado) {
+            powServo = fechado;
+            servoMotor.setPosition(powServo);
+        }
+        telemetry.addData("A potencia do motor do servo é de:", powServo);
+    }
 }
+
